a0057bf35706ef19d5ade4360c651223296d6328
Bill Murphy <murphy@litio.net>
2014-08-07 17:55:05 +0200

    Compile the initial changes

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '4bbb2c0a2d597c7ab65732488f6df1ff');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'eb465b9a6117948ea98284dc9a2e8130');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
