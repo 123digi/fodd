a41ff80fcd2795d1abf332ea0f5a24119e98f82a
rosday <sanudo@rodrigosanudo.com>
2014-08-19 12:43:01 +0200

    Delete home bottom widget

--- base.php
+++ base.php
@@ -31,14 +31,6 @@
     </div><!-- /.content -->
   </div><!-- /.wrap -->
 
-  <?php if ( is_front_page() && is_active_sidebar( 'sidebar-home-bottom' ) ) : ?>
-      <section class="jumbotron sidebar-full sidebar-bottom sidebar-home">
-         <div class="container">
-           <?php dynamic_sidebar('sidebar-home-bottom'); ?>
-         </div>
-      </section>
-  <?php endif; ?>
-
   <?php get_template_part('templates/footer'); ?>
 
 </body>
--- lib/custom/widgets.php
+++ lib/custom/widgets.php
@@ -17,14 +17,6 @@ function pith_widgets_init() {
     'after_title'   => '</h3>',
   ));
 
-  register_sidebar(array(
-    'name'          => __('Home Bottom', 'roots'),
-    'id'            => 'sidebar-home-bottom',
-    'before_widget' => '<section class="widget %1$s %2$s">',
-    'after_widget'  => '</section>',
-    'before_title'  => '<h3>',
-    'after_title'   => '</h3>',
-  ));
 //
 //  register_sidebar(array(
 //    'name'          => __('Store Top', 'roots'),
