7fc36ee3fb3d4e7159a6d7fce5a83422c3bb5642
Rodrigo Sañudo de Aymerich <rosday@Rodrigos-MacBook-Pro.local>
2014-08-21 13:51:26 +0200

    Styling blog

--- templates/content-single.php
+++ templates/content-single.php
@@ -1,5 +1,12 @@
 <?php while (have_posts()) : the_post(); ?>
   <article <?php post_class(); ?>>
+     <?php if ( has_post_thumbnail() ) { ?>
+      <figure>
+          <a href="<?php the_permalink(); ?>">
+              <?php the_post_thumbnail( 'post-custom-sidebar-thumbnail', array('class' => 'img-responsive') ); ?>
+          </a>
+      </figure>
+    <?php } ?>
     <header>
       <h1 class="entry-title"><?php the_title(); ?></h1>
       <?php get_template_part('templates/entry-meta'); ?>
@@ -8,7 +15,7 @@
       <?php the_content(); ?>
     </div>
     <footer>
-      <?php wp_link_pages(array('before' => '<nav class="page-nav"><p>' . __('Pages:', 'roots'), 'after' => '</p></nav>')); ?>
+
     </footer>
     <?php comments_template('/templates/comments.php'); ?>
   </article>
