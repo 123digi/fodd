17ced8de63974259ba110cbd9f0c0f4e6f0119f1
Rodrigo Sañudo de Aymerich <rosday@Rodrigos-MacBook-Pro.local>
2014-08-28 13:16:21 +0200

    Add template contact width widget-top

--- lib/custom/widgets.php
+++ lib/custom/widgets.php
@@ -84,6 +84,15 @@ function pith_widgets_init() {
     'after_title'   => '</h3>',
   ));
 
+  register_sidebar(array(
+    'name'          => __('Contact', 'roots'),
+    'id'            => 'widget-area-contact-top',
+    'before_widget' => '<section class="widget %1$s %2$s">',
+    'after_widget'  => '</section>',
+    'before_title'  => '<h3 class="widget-title">',
+    'after_title'   => '</h3>',
+  ));
+
 //
 //  register_sidebar(array(
 //    'name'          => __('Store Top', 'roots'),
